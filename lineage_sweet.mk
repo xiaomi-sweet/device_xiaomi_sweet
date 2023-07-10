7c93a35f9f9296eb41621b43e60f6644dc0eb59d
Kenan Daf <unmoved2121@gmail.com>
2023-07-10 23:37:34 +0000

    sweet: Go official

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -14,6 +14,26 @@ $(call inherit-product, device/xiaomi/sweet/device.mk)
 # Inherit some common Alphadroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := true
+TARGET_USE_PIXEL_LAUNCHER := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Gapps
+WITH_GAPPS := 2
+TARGET_USE_GOOGLE_TELEPHONY := true
+TARGET_USE_PIXEL_FRAMEWORK := true
+
+# Maintainer Flags
+ALPHA_MAINTAINER := UnmoveD
+ALPHA_BUILD_TYPE := Official
+
+# Debugging Flags
+TARGET_INCLUDE_MATLOG := false
+TARGET_DEFAULT_ADB_ENABLED := false
 
 PRODUCT_NAME := lineage_sweet
 PRODUCT_DEVICE := sweet
